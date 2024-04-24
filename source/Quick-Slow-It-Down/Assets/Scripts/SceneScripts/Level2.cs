482115a0a77db6df62fe11d0d5edf3751434c5bf
Dmitriy Ortyakov <109825093+dmitriyllama@users.noreply.github.com>
2024-04-24 18:14:23 +0300

    Added Level2 script

---
+++ source/Quick-Slow-It-Down/Assets/Scripts/SceneScripts/Level2.cs
@@ -0,0 +1,20 @@
+﻿namespace SceneScripts
+{
+    public class Level2 : Level
+    {
+        public override void ReactToPlayerAction()
+        {
+            
+        }
+
+        public override void ReactToPlayerDeath()
+        {
+            
+        }
+
+        public override void ReactToEnemyDeath()
+        {
+            
+        }
+    }
+}
