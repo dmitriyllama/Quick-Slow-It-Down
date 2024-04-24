79473ce08be8935fd85037aa12eb896d4c21f3a7
Yaroslav Prudnikov <barcarolla25@gmail.com>
2024-04-24 18:59:32 +0300

    created basic UI

--- source/Quick-Slow-It-Down/Assets/Scripts/FPSInput.cs
+++ source/Quick-Slow-It-Down/Assets/Scripts/FPSInput.cs
@@ -17,8 +17,8 @@ public class FPSInput : MonoBehaviour
     {
         charController = GetComponent<CharacterController>();
         
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
 
         level = GameObject.FindGameObjectWithTag("GameController").GetComponent<Level>();
         moveEvent = new UnityEvent();
---
+++ source/Quick-Slow-It-Down/Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+    [SerializeField] PauseScreen pauseScreen;
+    
+    void Start() {
+        pauseScreen.Close();
+        DontDestroyOnLoad(this);
+    }
+    
+    public void OnGamePause()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<MouseLookX>().enabled = false;
+        player.GetComponentInChildren<MouseLookY>().enabled = false;
+        player.GetComponent<FPSInput>().enabled = false;
+        Time.timeScale = 0;
+        pauseScreen.Open();
+    }
+}
