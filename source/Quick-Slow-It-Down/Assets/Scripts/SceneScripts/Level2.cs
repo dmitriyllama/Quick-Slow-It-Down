2370a07bcc0251dbe315258dc8e0d866bde543f6
Dmitriy Ortyakov <109825093+dmitriyllama@users.noreply.github.com>
2024-04-25 11:08:20 +0300

    Level implementations

--- source/Quick-Slow-It-Down/Assets/Scripts/SceneScripts/Level2.cs
+++ source/Quick-Slow-It-Down/Assets/Scripts/SceneScripts/Level2.cs
@@ -1,19 +1,106 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace SceneScripts
 {
     public class Level2 : Level
     {
+        [SerializeField] private GUIStyle guiStyle;
+        private string label;
+        private Camera cam;
+
+        private Coroutine activeCoroutine;
+        private int progress = 0;
+        private bool readThroughEnoughTutorial;
+        
+        [SerializeField] private List<EnemyAI.EnemyAI> enemies;
+        
         public override void ReactToPlayerAction()
         {
+            if (progress != 0) return;
+            progress = 1;
+            
+            label = "";
+        
+            StopCoroutine(activeCoroutine);
+            activeCoroutine = StartCoroutine(ActivateEnemy());
         }
 
         public override void ReactToPlayerDeath()
         {
+            StopCoroutine(activeCoroutine);
+            activeCoroutine = StartCoroutine(ResetLevel());
         }
 
         public override void ReactToEnemyDeath()
         {
+            progress++;
+
+            if (progress == 2)
+            {
+                StopCoroutine(activeCoroutine);
+                activeCoroutine = StartCoroutine(EndLevel());
+            }
+        }
+        
+        private IEnumerator StartLevel()
+        {
+            label = "Don't move!";
+            yield return new WaitForSeconds(3.2f);
+            guiStyle.fontSize = 24;
+            label = "Is anything familiar yet?";
+            yield return new WaitForSeconds(4.7f);
+        }
+        
+        private IEnumerator ActivateEnemy()
+        {
+            inAction = true;
+            foreach (var enemy in enemies)
+            {
+                enemy.active = true;
+            }
+            guiStyle.fontSize = 24;
+            yield return new WaitForSeconds(10.0f);
+            label = "You can't hide forever!";
+            yield return new WaitForSeconds(1.4f);
+            label = "There's a mission ahead of you!";
+        }
+        
+        private IEnumerator ResetLevel()
+        {
+            inAction = false;
+            yield return new WaitForSeconds(0.7f);
+            guiStyle.fontSize = 24;
+            label = "You have to be quicker than that!";
+            yield return new WaitForSeconds(1.2f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+        private IEnumerator EndLevel()
+        {
+            inAction = false;
+            yield return new WaitForSecondsRealtime(1.2f);
+            guiStyle.fontSize = 24;
+            label = "You got this!";
+            yield return new WaitForSeconds(1.2f);
+            SceneManager.LoadScene("Level3");
+        }
+        
+        void Start()
+        {
+            cam = Camera.main;
+            activeCoroutine = StartCoroutine(StartLevel());
+        }
+
+        private void OnGUI()
+        {
+            int w = 500;
+            int h = 100;
+            float posX = (cam.pixelWidth - w) / 2;
+            float posY = (cam.pixelHeight) / 4;
+            GUI.Label(new Rect(posX, posY, w, h), label, guiStyle);
         }
     }
 }
--- source/Quick-Slow-It-Down/Assets/Scripts/SceneScripts/Level3.cs
+++ source/Quick-Slow-It-Down/Assets/Scripts/SceneScripts/Level3.cs
@@ -1,20 +1,107 @@
-﻿namespace SceneScripts
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SceneScripts
 {
     public class Level3 : Level
     {
+        [SerializeField] private GUIStyle guiStyle;
+        private string label;
+        private Camera cam;
+
+        private Coroutine activeCoroutine;
+        private int progress = 0;
+        private bool readThroughEnoughTutorial;
+        
+        [SerializeField] private List<EnemyAI.EnemyAI> enemies;
+        
         public override void ReactToPlayerAction()
         {
-            throw new System.NotImplementedException();
+            if (progress != 0) return;
+            progress = 1;
+        
+            StopCoroutine(activeCoroutine);
+            activeCoroutine = StartCoroutine(ActivateEnemy());
         }
 
         public override void ReactToPlayerDeath()
         {
-            throw new System.NotImplementedException();
+            StopCoroutine(activeCoroutine);
+            activeCoroutine = StartCoroutine(ResetLevel());
         }
 
         public override void ReactToEnemyDeath()
         {
-            throw new System.NotImplementedException();
+            progress++;
+
+            if (progress == 4)
+            {
+                StopCoroutine(activeCoroutine);
+                activeCoroutine = StartCoroutine(EndLevel());
+            }
+        }
+        
+        private IEnumerator StartLevel()
+        {
+            yield return new WaitForSeconds(4.2f);
+            guiStyle.fontSize = 24;
+            label = "You need to be quick";
+        }
+        
+        private IEnumerator ActivateEnemy()
+        {
+            inAction = true;
+            foreach (var enemy in enemies)
+            {
+                enemy.active = true;
+            }
+            guiStyle.fontSize = 24;
+            yield return new WaitForSeconds(1.6f);
+            label = "The guards don't hesitate to keep this place secure";
+            yield return new WaitForSeconds(3.2f);
+            label = "Why don't they see the artifact in your hand?";
+            yield return new WaitForSeconds(3.4f);
+            label = "";
+        }
+        
+        private IEnumerator ResetLevel()
+        {
+            inAction = false;
+            yield return new WaitForSeconds(0.7f);
+            guiStyle.fontSize = 24;
+            label = "You have to be quicker!";
+            yield return new WaitForSeconds(1.2f);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+        private IEnumerator EndLevel()
+        {
+            inAction = false;
+            yield return new WaitForSecondsRealtime(1.2f);
+            guiStyle.fontSize = 24;
+            label = "We must move!";
+            yield return new WaitForSeconds(1.8f);
+            // FIXME Next transition
+            label = "";
+            Debug.Log("Demo done!");
+        }
+        
+        void Start()
+        {
+            cam = Camera.main;
+            activeCoroutine = StartCoroutine(StartLevel());
+        }
+
+        private void OnGUI()
+        {
+            int w = 500;
+            int h = 100;
+            float posX = (cam.pixelWidth - w) / 2;
+            float posY = (cam.pixelHeight) / 4;
+            GUI.Label(new Rect(posX, posY, w, h), label, guiStyle);
         }
     }
 }
